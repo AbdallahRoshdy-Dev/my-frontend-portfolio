49e0f3ca0a00d95ce3ecf92917a2da16d3a89b94
Abdullah Roshdy <abdullahroshdy000@gmail.com>
2026-06-16 20:47:08 +0300

    final upload

---
+++ GameArena/js/main.js
@@ -0,0 +1,11 @@
+// Output by alert
+// window.alert("haaaaaaaaaaaa");
+// Output by tag
+// document.getElementById('output').innerHTML="haaaaaaaaaaaa";
+// Output by conaole
+// console.log("haaaaaaa");
+
+// var prompt_container_1 = prompt();
+// var prompt_container_2 = prompt();
+
+// console.log(+prompt_container_1 + +prompt_container_1);
